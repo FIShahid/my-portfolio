55b72fd15222a158a2341b695acf88715beafd96
Fokhrul Islam <shahidcse12@gmail.com>
2022-06-10 03:04:43 +0600

    footer section added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import './Footer.css'
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
+import {BsFacebook} from 'react-icons/bs'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>Fokhrul Islam Shahid</a>
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+
+      </ul>
+      <div className="footer__socials">
+        <a href="https://www.linkedin.com/in/fokhrul-islam-shahid/"><BsLinkedin/></a>
+        <a href="https://github.com/FIShahid"><FaGithub/></a>
+        <a href="https://www.facebook.com/Undefined.sahid"><BsFacebook/></a>
+      </div>
+      <div className="footer__copyright">
+        <small>&copy; Fokhrul Islam Shahid. All rights reserved.</small>
+      </div>
+      </footer>
   )
 }
 
