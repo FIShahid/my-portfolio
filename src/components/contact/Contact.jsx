80c588f76e662821cc2b001378fe11bfd7d93fd0
Fokhrul Islam <shahidcse12@gmail.com>
2022-06-10 02:32:10 +0600

    contact section added

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,71 @@
-import React from 'react'
+import React, { useRef } from 'react';
 import './Contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import {RiMessengerLine} from 'react-icons/ri'
+import {BsWhatsapp} from 'react-icons/bs'
+
+import emailjs from 'emailjs-com';
+import { toast, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
 const Contact = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_6gi1amq', 'template_qblsjms', form.current, 'MVRrmyPMG6yFsLfE2')
+    
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      toast.success('Thank you for Reaching me, I will get in touch Soon..', {
+        position: toast.POSITION.TOP_CENTER,
+        autoClose: 8000
+      })
+      e.target.reset()
+  };
+
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className='contact__option'>
+            <MdOutlineEmail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>shahidcse12@gmail.com</h5>
+            <a href="mailto:shahidcse12@gmail.com" target="_blank">Send me a Message</a>
+
+          </article>
+
+          <article className='contact__option'>
+            <RiMessengerLine className='contact__option-icon'/>
+            <h4>Messenger</h4>
+            <h5>Fokhrul Islam Shahid</h5>
+            <a href="https://m.me/Undefined.sahid" target="_blank">Send me a Message</a>
+
+          </article>
+          <article className='contact__option'>
+            <BsWhatsapp className='contact__option-icon'/>
+            <h4>WhatsApp</h4>
+            <h5>+123456789</h5>
+            <a href="https://api.whatsapp.com/send?phone=+8801812521378" target="_blank">Send me a Message</a>
+
+          </article>
+
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name="email"  placeholder='Your Email' required />
+          <textarea name="message"  rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+      <ToastContainer/>
+      </section>
   )
 }
 
