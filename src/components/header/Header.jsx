e1f514325934d75541f9346c8e5c7312c366161e
Fokhrul Islam <shahidcse12@gmail.com>
2022-06-11 01:15:47 +0600

    error fixed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,20 +5,21 @@ import me3 from '../../assets/me3.png'
 import HeaderSocial from './HeaderSocial'
 const Header = () => {
   return (
-   <header>
-     <div className="container header__container">
-       <h5>Hello I'm</h5>
-       <h1>Fokhrul Islam Shahid</h1>
-       <h5 className='text-light'>Fullstack Developer</h5>
-       <CTA/>
-        <HeaderSocial/>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Fokhrul Islam Shahid</h1>
+        <h5 className='text-light'>Fullstack Developer</h5>
+        <CTA />
+        <HeaderSocial />
         <div className="me">
           <img src={me3} alt="" />
+
         </div>
         <a href="#contact" className='scroll__down'>Scroll Down</a>
 
-     </div>
-   </header>
+      </div>
+    </header>
   )
 }
 
--- src/components/header/HeaderSocial.jsx
+++ src/components/header/HeaderSocial.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import {BsLinkedin} from 'react-icons/bs'
-import {FaGithub} from 'react-icons/fa'
-import {BsFacebook} from 'react-icons/bs'
+import { BsLinkedin } from 'react-icons/bs'
+import { FaGithub } from 'react-icons/fa'
+import { BsFacebook } from 'react-icons/bs'
 
 const HeaderSocial = () => {
   return (
     <div className='header__socials'>
-        <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a>
-        <a href="https://github.com" target="_blank"><FaGithub/></a>
-        <a href="https://facebook.com" target="_blank"><BsFacebook/></a>
+
+      <a href="https://www.linkedin.com/in/fokhrul-islam-shahid/" target='blank'><BsLinkedin /></a>
+      <a href="https://github.com/FIShahid" target='blank'><FaGithub /></a>
+      <a href="https://www.facebook.com/Undefined.sahid" target='blank'><BsFacebook /></a>
 
     </div>
   )
